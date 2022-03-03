f95d5213dee674cf269b3313c163ac9e51f8ced3
Roland Gärtner <roland@gardeners.at>
2022-03-03 19:01:51 +0100

    blink built-in led with a FreeRTOS task

--- ExhaustControl/src/main.cpp
+++ ExhaustControl/src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
+static TaskHandle_t taskBlinkHandle;
+
+static void TaskBlink(void *pvParameters);
+
+// cppcheck-suppress unusedFunction
 void setup() {
-  // put your setup code here, to run once:
+  /**
+   * Task creation
+   */
+  xTaskCreate(TaskBlink, // Task function
+              "Blink", // Task name
+              128, // Stack size 
+              NULL, 
+              0, // Priority
+              &taskBlinkHandle); // Task handler
 }
 
+// cppcheck-suppress unusedFunction
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+}
+
+/* 
+ * Blink task. 
+ */
+static void TaskBlink(void *pvParameters)
+{
+  (void) pvParameters;
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for (;;)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    vTaskDelay( 250 / portTICK_PERIOD_MS );
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay( 250 / portTICK_PERIOD_MS );
+  }
 }
